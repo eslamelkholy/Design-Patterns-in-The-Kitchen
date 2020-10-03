838c19ac90b057989cd0a1b1f6b119e1d252b80b
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-10-03 14:56:55 +0200

    Added Class Pattern With Javasciprt Code

---
+++ Creational Patterns/1-Class Pattern.js
@@ -0,0 +1,11 @@
+class Car {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+}
+
+const civic = new Car(4, "A10", "green");
+
+console.log(civic);
