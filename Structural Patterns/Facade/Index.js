1eefbb71689937a1d7e6ce0512f8c3b2e681fa02
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-10-05 20:26:29 +0200

    Added Facade Design Pattern With Simple Example Using React

---
+++ Structural Patterns/Facade/Index.js
@@ -0,0 +1,29 @@
+/**
+ * Anyone Using React/Angular,... Developer Already Used It
+ * it's Really Very Simple and make Code more Readable
+ * E.G This Very Simple Code Where i Rendre the App Component into Another file
+ */
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+<App />;
