df49e9aa736840b8e51b10415675b7736a3dd441
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-10-05 19:52:03 +0200

    Setup Nodejs Server To Work With Modules Design Pattern

---
+++ Structural Patterns/Modules/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
