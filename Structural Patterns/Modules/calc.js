6af7aa4aa9f18a3811028a10bc216f453dcdf3fc
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-10-05 19:58:16 +0200

    Implement Modules Design Patterns With Nodejs Server

---
+++ Structural Patterns/Modules/calc.js
@@ -0,0 +1,3 @@
+const calc = () => 4 * 3;
+
+export default calc;
--- Structural Patterns/Modules/index.js
+++ Structural Patterns/Modules/index.js
@@ -1,12 +1,15 @@
-import express from 'express';
+/**
+ * Modules Design Pattern
+ * This Pattern is Popular Like Import/Export
+ * Through Encapsulating a Block of Code into Pure Function  Separate Module
+ */
+
+import express from "express";
+import calc from "./calc";
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
-);
+app.get("/", (req, res) => res.send(`Modules Design Pattern Goes Here, ${calc()}`));
 
-app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
