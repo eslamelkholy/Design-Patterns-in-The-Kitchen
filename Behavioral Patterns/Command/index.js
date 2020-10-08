3e8e1488d4bf151c5151fe734e5f61eb08e29507
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-10-08 21:06:06 +0200

    Added Command Pattern and How That Abstract the Function From Execution Action it Self that Updates Reducers in Redux

---
+++ Behavioral Patterns/Command/index.js
@@ -0,0 +1,10 @@
+/**
+ * Command Design Pattern
+ * Encapsulates Actions Or Operators as Object
+ * in Other Word Abstract the Function Or The Execution Of The Function From the Action It Self
+ * The Best Example Here for It The Redux
+ * That Separate the Action From The Execution That updated the Reducers
+ * it's also often has Been Using In Functional Programming
+ */
+
+// A Great Example Can Describe all of this https://redux.js.org/basics/examples
